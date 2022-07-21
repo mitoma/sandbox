be464fd890359afb2b6a4f9635c60d99c21fcb50
mitoma <mutetheradio@gmail.com>
2022-07-22 06:45:08 +0900

    izugo initial commit

---
+++ rust/izugo/src/main.rs
@@ -0,0 +1,31 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[clap(author, version, about = "Version calcurator based on source code.", long_about = None)]
+pub(crate) struct Args {
+    // Command
+    #[clap(default_value = "uptime")]
+    command: String,
+}
+
+fn main() {
+    let args = Args::parse();
+    let proc = std::process::Command::new(args.command).output().unwrap();
+    let stdout = if proc.stdout.is_empty() {
+        None
+    } else {
+        Some(String::from_utf8(proc.stdout).unwrap())
+    };
+    let stderr = if proc.stderr.is_empty() {
+        None
+    } else {
+        Some(String::from_utf8(proc.stderr).unwrap())
+    };
+
+    if let Some(out) = stdout {
+        println!("STDOUT:{}", out);
+    }
+    if let Some(out) = stderr {
+        println!("STDERR:{}", out);
+    }
+}
