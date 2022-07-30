1a5b2384383e734f8d33e7ff1c5e39144bf497f0
mitoma <mutetheradio@gmail.com>
2022-07-31 06:39:48 +0900

    Box::leak を使って Args は解放されないようにしておく。
    'static lifetime にしておかないと default_handler のクロージャに引数を持っていけないため

--- mitoma-org/backend/src/main.rs
+++ mitoma-org/backend/src/main.rs
@@ -8,6 +8,10 @@ use clap::Parser;
 #[derive(Parser, Debug)]
 #[clap(author, version, about = "version calucurator for git repository", long_about = None)]
 struct Args {
+    // address
+    #[clap(short, long, default_value = "127.0.0.1")]
+    address: String,
+
     // port number
     #[clap(short, long, default_value = "8080")]
     port: u16,
@@ -19,26 +23,22 @@ struct Args {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let args = Args::parse();
-    let static_file_path = args.static_file_path.clone();
-    HttpServer::new(move || {
-        App::new()
-            .service(web::scope("/api").service(health))
-            .service(
-                Files::new("/", &static_file_path)
-                    .index_file("index.html")
-                    .default_handler(|req: ServiceRequest| async {
-                        let (req, _) = req.into_parts();
-                        let file =
-                            NamedFile::open_async(format!("{}/index.html", "../frontend/build"))
-                                .await?;
-                        let res = file.into_response(&req);
-                        Ok(ServiceResponse::new(req, res))
-                    })
-                    .redirect_to_slash_directory(),
-            )
+    let args = Box::leak(Args::parse().into());
+    HttpServer::new(|| {
+        let api_app = web::scope("/api").service(health);
+        let static_file_app = Files::new("/", &args.static_file_path)
+            .index_file("index.html")
+            .default_handler(|req: ServiceRequest| async {
+                let (req, _) = req.into_parts();
+                let file = NamedFile::open_async(&format!("{}/index.html", &args.static_file_path))
+                    .await?;
+                let res = file.into_response(&req);
+                Ok(ServiceResponse::new(req, res))
+            })
+            .redirect_to_slash_directory();
+        App::new().service(api_app).service(static_file_app)
     })
-    .bind(("127.0.0.1", args.port))?
+    .bind((args.address.as_str(), args.port))?
     .run()
     .await
 }
