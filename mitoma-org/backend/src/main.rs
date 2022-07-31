d531565a42f6565a7a14c7e46c43ed90149ed42c
mitoma <mutetheradio@gmail.com>
2022-08-01 06:58:11 +0900

    content 機能の実装

--- mitoma-org/backend/src/main.rs
+++ mitoma-org/backend/src/main.rs
@@ -1,11 +1,15 @@
+use std::path::PathBuf;
+
 use actix_files::{Files, NamedFile};
 use actix_web::{
     dev::{ServiceRequest, ServiceResponse},
     get,
     http::header::ContentType,
-    web, App, HttpResponse, HttpServer, Responder,
+    web, App, HttpRequest, HttpResponse, HttpServer, Responder,
 };
 use clap::Parser;
+use pulldown_cmark::{html, Options};
+use serde::{Deserialize, Serialize};
 
 #[derive(Parser, Debug)]
 #[clap(author, version, about = "version calucurator for git repository", long_about = None)]
@@ -21,13 +25,17 @@ struct Args {
     // static file path
     #[clap(short, long, default_value = "../frontend/build")]
     static_file_path: String,
+
+    // content file path
+    #[clap(short, long, default_value = "../contents")]
+    contents_file_path: String,
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     let args = Box::leak(Args::parse().into());
     HttpServer::new(|| {
-        let api_app = web::scope("/api").service(health);
+        let api_app = web::scope("/api").service(health).service(content);
         let static_file_app = Files::new("/", &args.static_file_path)
             .index_file("index.html")
             .default_handler(|req: ServiceRequest| async {
@@ -50,4 +58,43 @@ async fn health() -> impl Responder {
     HttpResponse::Ok()
         .append_header(ContentType(mime::APPLICATION_JSON))
         .body(r#"{"__html": "<b>OK</b>"}"#)
+}
+
+#[derive(Deserialize)]
+struct ContentPath {
+    content_path: String,
+}
+
+#[derive(Serialize)]
+struct ContentOutput {
+    pub(crate) html: String,
+}
+
+#[get("/v1/content/{content_path:.*}")]
+async fn content(path: web::Path<ContentPath>, req: HttpRequest) -> impl Responder {
+    let options = Options::empty();
+    let mut path_buf = PathBuf::new();
+    path_buf.push("../contents");
+    let mut md_path_buf = path_buf.clone().canonicalize().unwrap();
+    md_path_buf.push(format!("{}.md", &path.content_path));
+
+    // TODO ディレクトリトラバーサルのチェック
+    if let Ok(input) = std::fs::read_to_string(md_path_buf.as_path()) {
+        let parser = pulldown_cmark::Parser::new_ext(&input, options);
+        let mut html: String = String::with_capacity(input.len() * 3 / 2);
+        html::push_html(&mut html, parser);
+        let resp = ContentOutput { html };
+        let resp_string = serde_json::to_string(&resp).unwrap();
+        return HttpResponse::Ok()
+            .append_header(ContentType(mime::APPLICATION_JSON))
+            .body(resp_string);
+    }
+
+    let mut blob_path_buf = path_buf.canonicalize().unwrap();
+    blob_path_buf.push(&path.content_path);
+    if let Ok(file) = NamedFile::open_async(blob_path_buf.as_path()).await {
+        return file.into_response(&req);
+    }
+
+    HttpResponse::NotFound().finish()
 }
