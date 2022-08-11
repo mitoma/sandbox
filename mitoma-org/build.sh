dae754f9670d49507819daefaab4a916e7737591
mitoma <mutetheradio@gmail.com>
2022-08-11 16:41:25 +0900

    artifact を workflow から取得するか無ければ取ってくる

--- mitoma-org/build.sh
+++ mitoma-org/build.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 set -eux
 
-RUN_ID="$1"
 ARTIFACT_DIR=artifact
-rm -rf "$ARTIFACT_DIR"
-mkdir "$ARTIFACT_DIR"
 REPO_ROOT=$(git rev-parse --show-toplevel)
 MITOMA_ORG_VERSION=$(sver calc "$REPO_ROOT/mitoma-org")
 BACKEND_VERSION=$(sver calc "$REPO_ROOT/mitoma-org/backend")
 FRONTEND_VERSION=$(sver calc "$REPO_ROOT/mitoma-org/frontend")
 
-gh run download -n "mitoma-org-backend-${BACKEND_VERSION}" --dir "$ARTIFACT_DIR/backend" "$RUN_ID"
-chmod +x "$ARTIFACT_DIR/backend/backend"
+if [ ! -d "$ARTIFACT_DIR" ]; then
+  mkdir "$ARTIFACT_DIR"
+fi
 
-gh run download -n "mitoma-org-frontend-${FRONTEND_VERSION}" --dir "$ARTIFACT_DIR/frontend" "$RUN_ID"
+if [ ! -d "$ARTIFACT_DIR/backend" ]; then
+  gh run download -n "mitoma-org-backend-${BACKEND_VERSION}" --dir "$ARTIFACT_DIR/backend"
+fi
+chmod +x "$ARTIFACT_DIR/backend/backend"
+  
+if [ ! -d "$ARTIFACT_DIR/frontend" ]; then
+  gh run download -n "mitoma-org-frontend-${FRONTEND_VERSION}" --dir "$ARTIFACT_DIR/frontend"
+fi
 
 docker build --build-arg ARTIFACT_DIR="$ARTIFACT_DIR" -t "mitoma-org:$MITOMA_ORG_VERSION" .
