e8b0b2b37428bc9d4f9490b7a512b0fde5b3f2e6
mitoma <mutetheradio@gmail.com>
2022-08-11 15:57:31 +0900

    ワークフロー中の Artifact を取得できるように修正

--- mitoma-org/build.sh
+++ mitoma-org/build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -eux
 
+RUN_ID="$1"
 ARTIFACT_DIR=artifact
 rm -rf "$ARTIFACT_DIR"
 mkdir "$ARTIFACT_DIR"
@@ -9,9 +10,9 @@ MITOMA_ORG_VERSION=$(sver calc "$REPO_ROOT/mitoma-org")
 BACKEND_VERSION=$(sver calc "$REPO_ROOT/mitoma-org/backend")
 FRONTEND_VERSION=$(sver calc "$REPO_ROOT/mitoma-org/frontend")
 
-gh run download -n "mitoma-org-backend-${BACKEND_VERSION}" --dir "$ARTIFACT_DIR/backend"
+gh run download -n "mitoma-org-backend-${BACKEND_VERSION}" --dir "$ARTIFACT_DIR/backend" "$RUN_ID"
 chmod +x "$ARTIFACT_DIR/backend/backend"
 
-gh run download -n "mitoma-org-frontend-${FRONTEND_VERSION}" --dir "$ARTIFACT_DIR/frontend"
+gh run download -n "mitoma-org-frontend-${FRONTEND_VERSION}" --dir "$ARTIFACT_DIR/frontend" "$RUN_ID"
 
 docker build --build-arg ARTIFACT_DIR="$ARTIFACT_DIR" -t "mitoma-org:$MITOMA_ORG_VERSION" .
