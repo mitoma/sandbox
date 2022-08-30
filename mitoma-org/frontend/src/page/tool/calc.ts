1a999b03c569d619fe0d612f4656be932fe64e74
mitoma <mutetheradio@gmail.com>
2022-08-30 15:06:12 +0000

    日付周りのテストを書いた

--- mitoma-org/frontend/src/page/tool/calc.ts
+++ mitoma-org/frontend/src/page/tool/calc.ts
@@ -1,21 +1,29 @@
+import { LocalSee } from "@mui/icons-material";
+import { formatDuration, intervalToDuration } from "date-fns";
+
 export function dateToJapaneseFormatString(date: Date): string {
   const eraFormat = new Intl.DateTimeFormat("ja-JP-u-ca-japanese", {
     era: "short",
     year: "numeric",
     month: "long",
-    day: "2-digit",
+    day: "numeric",
   });
   return eraFormat.format(date);
 }
 
-export function dateString(date: Date): string {
+export function dateToString(date: Date): string {
   const adFormat = new Intl.DateTimeFormat("ja-JP", {
     era: "short",
     year: "numeric",
     month: "long",
-    day: "2-digit",
+    day: "numeric",
   });
   return adFormat.format(date);
+}
+
+export function intervalString(start: Date, end: Date): string {
+  const duration = intervalToDuration({ start, end });
+  return `${duration.years} 年 ${duration.months} カ月 ${duration.days} 日`;
 }
 
 export function pastYearDays(targetDate: Date): string {
