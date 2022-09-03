c2bea612824c56fb70f996e0b46d25df2034d56d
mitoma <mutetheradio@gmail.com>
2022-09-03 07:32:29 +0000

    ドメイン毎に別の実装を返すように修正

--- mitoma-org/backend/src/lib.rs
+++ mitoma-org/backend/src/lib.rs
@@ -21,4 +21,8 @@ pub struct Args {
     // content file path
     #[clap(short, long, default_value = "../contents")]
     pub contents_file_path: String,
+
+    // static file path for hello.mitoma.org
+    #[clap(long, default_value = "../contents_hello")]
+    pub static_file_path_for_hello: String,
 }
