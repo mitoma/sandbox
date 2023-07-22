8a1628e5de264d9a3c361b0f330cf4f18b34c3f0
mitoma <mutetheradio@gmail.com>
2023-07-22 09:09:45 +0900

    create react app を消した

--- mitoma-org/frontend-bak/src/api/fetchAboutMe.ts
+++
@@ -1,9 +0,0 @@
-type AboutMeResp = {
-  html: string;
-};
-
-async function fetchAboutMe(): Promise<AboutMeResp> {
-  return fetch("/api/v1/content/about_me", { method: "GET" }).then((res) => res.json());
-}
-
-export default fetchAboutMe;
