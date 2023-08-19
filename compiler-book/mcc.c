a95aec204bade7f06a47847a4ffbc7a595af068e
mitoma <mutetheradio@gmail.com>
2023-08-19 02:43:44 +0000

    エラーメッセージを出力できるようにする

--- compiler-book/mcc.c
+++ compiler-book/mcc.c
@@ -24,6 +24,22 @@ struct Token
 
 Token *token;
 
+char *user_input;
+
+void error_at(char *loc, char *fmt, ...)
+{
+    va_list ap;
+    va_start(ap, fmt);
+
+    int pos = loc - user_input;
+    fprintf(stderr, "%s\n", user_input);
+    fprintf(stderr, "%*s", pos, " ");
+    fprintf(stderr, "^ ");
+    vfprintf(stderr, fmt, ap);
+    fprintf(stderr, "\n");
+    exit(1);
+}
+
 void error(char *fmt, ...)
 {
     va_list ap;
@@ -47,7 +63,7 @@ void expect(char op)
 {
     if (token->kind != TK_RESERVED || token->str[0] != op)
     {
-        error("'%c' ではありません", op);
+        error_at(token->str, "'%c' ではありません", op);
     }
     token = token->next;
 }
@@ -56,7 +72,7 @@ int expect_number()
 {
     if (token->kind != TK_NUM)
     {
-        error("数ではありません");
+        error_at(token->str, "数ではありません");
     }
     int val = token->val;
     token = token->next;
@@ -101,8 +117,7 @@ Token *tokenize(char *p)
             cur->val = strtol(p, &p, 10);
             continue;
         }
-
-        error("トークナイズできません, %c", *p);
+        error_at(p, "トークナイズできません");
     }
     new_token(TK_EOF, cur, p);
     return head.next;
@@ -116,7 +131,8 @@ int main(int argc, char **argv)
         return 1;
     }
 
-    token = tokenize(argv[1]);
+    user_input = argv[1];
+    token = tokenize(user_input);
 
     printf(".intel_syntax noprefix\n");
     printf(".globl main\n");
