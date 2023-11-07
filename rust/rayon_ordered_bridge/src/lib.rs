2cd1e82286071636694b48c01cb648c8a583b26e
ryo-mitoma <ryo-mitoma@cybozu.co.jp>
2023-11-07 23:57:10 +0000

    良い感じになった

--- rust/rayon_ordered_bridge/src/lib.rs
+++ rust/rayon_ordered_bridge/src/lib.rs
@@ -1,60 +1,60 @@
 use rayon::prelude::*;
 use std::{
-    sync::mpsc::{channel, sync_channel, Receiver, SyncSender},
+    sync::mpsc::{channel, sync_channel, Receiver},
     thread::spawn,
 };
 
-struct OrderedBridge<T, U, F>
-where
-    T: Send + Sync,
-    U: Send + Sync,
-    F: Fn(T) -> U + Send + Sync,
-{
-    source: Receiver<T>,
-    collect_tx: SyncSender<Receiver<U>>,
-    collect_rx: Receiver<Receiver<U>>,
-    closure: F,
+pub trait OrderedParallelBridge: Sized {
+    /// Creates a bridge from this type to a `ParallelIterator`.
+    fn ordering_par_bridge(self) -> OrderedIterBridge<Self>;
 }
 
-impl<T, U, F> OrderedBridge<T, U, F>
+impl<T: Iterator + Send> OrderedParallelBridge for T
 where
-    T: Send + Sync + 'static,
-    U: Send + Sync + 'static,
-    F: Fn(T) -> U,
-    F: Send + Sync + 'static,
+    T::Item: Send,
 {
-    fn new(source: Receiver<T>, closure: F, bound: usize) -> Self {
-        let (collect_tx, collect_rx) = sync_channel::<Receiver<U>>(bound);
-        Self {
-            source,
-            collect_tx,
-            collect_rx,
-            closure,
-        }
+    fn ordering_par_bridge(self) -> OrderedIterBridge<Self> {
+        OrderedIterBridge { iter: self }
     }
+}
 
-    fn calc(self) -> Receiver<U>
+#[derive(Debug, Clone)]
+pub struct OrderedIterBridge<Iter> {
+    iter: Iter,
+}
+
+impl<Iter: Iterator + Send + 'static> OrderedIterBridge<Iter>
+where
+    Iter::Item: Send,
+{
+    pub fn parallel_map_with_order<F, U>(self, bound: usize, func: F) -> Receiver<U>
     where
-        F: Fn(T) -> U + Send + Sync,
+        F: Fn(Iter::Item) -> U,
+        F: Send + Sync + 'static,
+        U: Send + Sync + 'static,
     {
+        let (collect_tx, collect_rx) = sync_channel::<Receiver<U>>(bound);
         let (result_tx, result_rx) = channel::<U>();
 
+        let source = self.iter;
+
         spawn(move || {
-            self.source
+            source
                 .into_iter()
-                .map(|v| {
+                .map(|v: <Iter as Iterator>::Item| {
                     let (s, r) = channel::<U>();
-                    self.collect_tx.send(r).unwrap();
+                    collect_tx.send(r).unwrap();
                     (v, s)
                 })
                 .par_bridge()
                 .for_each(|(v, s)| {
-                    let r = (self.closure)(v);
+                    let r = (func)(v);
                     s.send(r).unwrap();
                 });
         });
+
         spawn(move || {
-            self.collect_rx.iter().for_each(|r| {
+            collect_rx.iter().for_each(|r| {
                 result_tx.send(r.recv().unwrap()).unwrap();
             });
         });
@@ -100,7 +100,7 @@ where
 mod tests {
     use std::{sync::mpsc::channel, thread::sleep, time::Duration};
 
-    use crate::calc;
+    use crate::{calc, OrderedParallelBridge};
 
     #[test]
     fn test_calc() {
@@ -124,5 +124,19 @@ mod tests {
         result.iter().for_each(|r| {
             println!("{}", r);
         });
+    }
+
+    #[test]
+
+    fn test_ordering_par_bridge() {
+        let sut = (1..1000).ordering_par_bridge();
+        sut.parallel_map_with_order(100, |i| {
+            sleep(Duration::from_millis(1000 - i));
+            i * 2
+        })
+        .iter()
+        .for_each(|r| {
+            println!("{}", r);
+        })
     }
 }
