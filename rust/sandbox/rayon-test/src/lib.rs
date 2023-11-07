dfcbe4377849175caea7a3babe4ee06041914986
ryo-mitoma <ryo-mitoma@cybozu.co.jp>
2023-11-07 07:31:56 +0000

    テストケースを追加

--- rust/sandbox/rayon-test/src/lib.rs
+++ rust/sandbox/rayon-test/src/lib.rs
@@ -1,10 +1,31 @@
 #[cfg(test)]
 mod tests {
     use std::{
-        sync::mpsc::{channel, Receiver, Sender},
-        thread::sleep,
+        sync::mpsc::{channel, sync_channel, Receiver, Sender},
+        thread::{self, sleep},
         time::Duration,
     };
+
+    #[test]
+    fn test_array() {
+        let array = vec![1, 2, 3, 4, 5, 6, 7, 8, 9];
+        let mut result: Vec<u64> = vec![];
+
+        use rayon::prelude::*;
+        array
+            .par_iter()
+            .map(|x| {
+                // calc
+                sleep(Duration::from_millis(1000 - (x * 100)));
+                *x
+            })
+            .inspect(|x| println!("{}", x) /* 降順で出力される */)
+            .collect_into_vec(&mut result);
+
+        result.iter().for_each(|x| {
+            println!("{}", x); /* 昇順で出力される */
+        });
+    }
 
     #[test]
     fn test_for_each() {
@@ -79,6 +100,48 @@ mod tests {
             .for_each(|(x, sender)| {
                 sender.send(x).unwrap();
             });
+
+        collect_rx.into_iter().for_each(|r| {
+            println!("{}", r.recv().unwrap());
+        });
+    }
+
+    // rayon で重たい計算処理は並列処理しつつ、結果の受け取りは順序保障する。また、同時処理数を制限する。
+    #[test]
+    fn test_chan_chan_bounded() {
+        let (source_rx, collect_rx) = {
+            // 最初のストリームを表す channel
+            let (source_tx, source_rx) = channel::<(u64, Sender<u64>)>();
+            // 処理後のストリームを表す channel
+            let (collect_tx, collect_rx) = sync_channel::<Receiver<u64>>(5);
+
+            thread::spawn(move || {
+                (1..10).for_each(|x| {
+                    println!("store: {}", x);
+                    let (t, r) = channel::<u64>();
+                    source_tx.send((x, t)).unwrap();
+                    collect_tx.send(r).unwrap();
+                });
+            });
+
+            (source_rx, collect_rx)
+        };
+
+        thread::spawn(move || {
+            use rayon::prelude::*;
+            source_rx
+                .into_iter()
+                .par_bridge()
+                .map(|(x, sender)| {
+                    println!("calc: {}", x);
+                    // ここが重たい計算計算処理
+                    sleep(Duration::from_millis(1000 - (x * 100)));
+                    (x, sender)
+                })
+                .for_each(|(x, sender)| {
+                    sender.send(x).unwrap();
+                });
+        });
 
         collect_rx.into_iter().for_each(|r| {
             println!("{}", r.recv().unwrap());
