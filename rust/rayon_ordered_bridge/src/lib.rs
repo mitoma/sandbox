6f6235378c2a691af5e071734eb4890e7f6697ef
mitoma <mutetheradio@gmail.com>
2023-11-08 14:38:00 +0000

    bounded_parralel_mapped_channel を実装

--- rust/rayon_ordered_bridge/src/lib.rs
+++ rust/rayon_ordered_bridge/src/lib.rs
@@ -1,9 +1,44 @@
 use std::{
-    sync::mpsc::{channel, sync_channel, Receiver},
+    sync::mpsc::{channel, sync_channel, Receiver, SyncSender},
     thread::spawn,
 };
 
 use rayon::prelude::{ParallelBridge, ParallelIterator};
+
+pub fn bounded_parralel_mapped_channel<T, U, F>(bound: usize, f: F) -> (SyncSender<T>, Receiver<U>)
+where
+    T: Send + 'static,
+    U: Send + 'static,
+    F: Fn(T) -> U,
+    F: Send + Sync + 'static,
+{
+    let (source_tx, source_rx) = sync_channel::<T>(0);
+    let (collect_tx, collect_rx) = sync_channel::<Receiver<U>>(bound);
+    let (result_tx, result_rx) = sync_channel::<U>(bound);
+
+    spawn(move || {
+        source_rx
+            .into_iter()
+            .map(|v| {
+                let (s, r) = channel::<U>();
+                collect_tx.send(r).unwrap();
+                (v, s)
+            })
+            .par_bridge()
+            .for_each(|(v, s)| {
+                let r = f(v);
+                s.send(r).unwrap();
+            });
+    });
+
+    spawn(move || {
+        collect_rx.iter().for_each(|r| {
+            result_tx.send(r.recv().unwrap()).unwrap();
+        });
+    });
+
+    (source_tx, result_rx)
+}
 
 pub trait OrderedParallelBridge: Sized {
     fn ordered_parallel_receiver(self) -> OrderedParallelReceiver<Self>;
@@ -63,7 +98,27 @@ mod tests {
         time::Duration,
     };
 
-    use crate::OrderedParallelBridge;
+    use crate::{bounded_parralel_mapped_channel, OrderedParallelBridge};
+
+    #[test]
+    fn test_bounded_parralel_map_channel() {
+        let result_rx = {
+            let (source_tx, result_rx) = bounded_parralel_mapped_channel(2, |x: u64| {
+                sleep(Duration::from_millis(1000u64 - x * 100));
+                x.to_string()
+            });
+            std::thread::spawn(move || {
+                (1..10).for_each(|x| {
+                    source_tx.send(x).unwrap();
+                });
+            });
+            result_rx
+        };
+
+        result_rx.iter().for_each(|r| {
+            println!("{}", r);
+        });
+    }
 
     #[test]
     fn test_ordering_par_bridge() {
