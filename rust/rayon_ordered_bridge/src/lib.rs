8f48f62b47891045876b48cf8d395d4c46d47aa9
mitoma <mutetheradio@gmail.com>
2023-11-09 02:47:14 +0000

    スレッドを rayon にまとめてライフタイムを 'static ではないようにする

--- rust/rayon_ordered_bridge/src/lib.rs
+++ rust/rayon_ordered_bridge/src/lib.rs
@@ -1,22 +1,27 @@
-use std::{
-    sync::mpsc::{channel, sync_channel, Receiver, SyncSender},
-    thread::spawn,
+use std::sync::mpsc::{channel, sync_channel, Receiver, SyncSender};
+
+use rayon::{
+    prelude::{ParallelBridge, ParallelIterator},
+    Scope,
 };
 
-use rayon::prelude::{ParallelBridge, ParallelIterator};
-
-fn internal_bounded_parralel<I, F, T, U>(bound: usize, iter: I, f: F) -> Receiver<U>
+pub fn bounded_parralel_map<'a, I, F, T, U>(
+    s: &Scope<'a>,
+    bound: usize,
+    iter: I,
+    f: F,
+) -> Receiver<U>
 where
-    I: Iterator<Item = T> + Send + 'static,
+    I: Iterator<Item = T> + Send + 'a,
     F: Fn(T) -> U,
-    F: Send + Sync + 'static,
+    F: Send + Sync + 'a,
     T: Send,
-    U: Send + 'static,
+    U: Send + 'a,
 {
     let (collect_tx, collect_rx) = sync_channel::<Receiver<U>>(bound);
     let (result_tx, result_rx) = sync_channel::<U>(bound);
 
-    spawn(move || {
+    s.spawn(move |_| {
         iter.map(|v| {
             let (s, r) = channel::<U>();
             collect_tx.send(r).unwrap();
@@ -29,7 +34,7 @@ where
         });
     });
 
-    spawn(move || {
+    s.spawn(move |_| {
         collect_rx.iter().for_each(|r| {
             result_tx.send(r.recv().unwrap()).unwrap();
         });
@@ -38,32 +43,37 @@ where
     result_rx
 }
 
-pub fn bounded_parralel_mapped_iterator<I, F, T, U>(
+pub fn bounded_parralel_map_iterator<'a, I, F, T, U>(
+    s: &Scope<'a>,
     bound: usize,
     iter: I,
     f: F,
 ) -> impl Iterator<Item = U>
 where
-    I: Iterator<Item = T> + Send + 'static,
+    I: Iterator<Item = T> + Send + 'a,
     F: Fn(T) -> U,
-    F: Send + Sync + 'static,
+    F: Send + Sync + 'a,
     T: Send,
-    U: Send + 'static,
+    U: Send + 'a,
 {
-    internal_bounded_parralel(bound, iter, f).into_iter()
+    bounded_parralel_map(s, bound, iter, f).into_iter()
 }
 
-pub fn bounded_parralel_mapped_channel<T, U, F>(bound: usize, f: F) -> (SyncSender<T>, Receiver<U>)
+pub fn bounded_parralel_map_channel<'a, T, U, F>(
+    s: &Scope<'a>,
+    bound: usize,
+    f: F,
+) -> (SyncSender<T>, Receiver<U>)
 where
     F: Fn(T) -> U,
-    F: Send + Sync + 'static,
-    T: Send + 'static,
-    U: Send + 'static,
+    F: Send + Sync + 'a,
+    T: Send + 'a,
+    U: Send + 'a,
 {
     let (source_tx, source_rx) = sync_channel::<T>(0);
     (
         source_tx,
-        internal_bounded_parralel(bound, source_rx.into_iter(), f),
+        bounded_parralel_map(s, bound, source_rx.into_iter(), f),
     )
 }
 
@@ -71,33 +81,40 @@ where
 mod tests {
     use std::{thread::sleep, time::Duration};
 
-    use crate::{bounded_parralel_mapped_channel, bounded_parralel_mapped_iterator};
+    use rayon::scope;
+
+    use crate::{bounded_parralel_map_channel, bounded_parralel_map_iterator};
 
     #[test]
     fn test_bounded_parralel_map_channel() {
-        let result_rx = {
-            let (source_tx, result_rx) = bounded_parralel_mapped_channel(2, |x: u64| {
-                sleep(Duration::from_millis(1000u64 - x * 100));
-                x.to_string()
-            });
-            std::thread::spawn(move || {
-                (1..10).for_each(|x| {
-                    source_tx.send(x).unwrap();
+        scope(|s| {
+            let result_rx = {
+                let (source_tx, result_rx) = bounded_parralel_map_channel(s, 2, |x: u64| {
+                    sleep(Duration::from_millis(1000u64 - x * 100));
+                    x.to_string()
                 });
-            });
-            result_rx
-        };
+                s.spawn(move |_| {
+                    (1..10).for_each(|x| {
+                        source_tx.send(x).unwrap();
+                    });
+                });
+                result_rx
+            };
 
-        result_rx.iter().for_each(|r| {
-            println!("{}", r);
+            result_rx.iter().for_each(|r| {
+                println!("{}", r);
+            });
         });
     }
 
     #[test]
     fn test_bounded_parralel_map_iter() {
-        let sut = vec![1, 2, 3, 4, 5, 6, 7];
-        let result: Vec<i32> =
-            bounded_parralel_mapped_iterator(3, sut.into_iter(), |x| x * x).collect();
-        assert!(result == vec![1, 4, 9, 16, 25, 36, 49]);
+        scope(|s| {
+            let sut = vec![1, 2, 3, 4, 5, 6, 7];
+
+            let result: Vec<i32> =
+                bounded_parralel_map_iterator(s, 3, sut.into_iter(), |x| x * x).collect();
+            assert!(result == vec![1, 4, 9, 16, 25, 36, 49]);
+        });
     }
 }
