508b5aab7df3c062041bd034ea846c862a4688f9
mitoma <mutetheradio@gmail.com>
2023-11-12 02:49:35 +0000

    scope を使う実装は一旦無くす

--- rust/rayon_ordered_bridge/src/lib.rs
+++ rust/rayon_ordered_bridge/src/lib.rs
@@ -1,11 +1,20 @@
-use std::sync::mpsc::{channel, sync_channel, Receiver, SyncSender};
+use std::sync::mpsc::{channel, sync_channel, Receiver, SyncSender, TrySendError};
 
-use rayon::{
-    prelude::{ParallelBridge, ParallelIterator},
-    Scope,
-};
+use rayon::prelude::{ParallelBridge, ParallelIterator};
 
-pub fn bounded_parralel_map<I, F, T, U>(bound: usize, iter: I, f: F) -> Receiver<U>
+pub struct Waiter {
+    handlers: Vec<std::thread::JoinHandle<()>>,
+}
+
+impl Waiter {
+    pub fn wait(self) {
+        for handler in self.handlers {
+            handler.join().unwrap();
+        }
+    }
+}
+
+pub fn bounded_parralel_map<I, F, T, U>(bound: usize, iter: I, f: F) -> (Receiver<U>, Waiter)
 where
     I: Iterator<Item = T> + Send + 'static,
     F: Fn(T) -> U,
@@ -16,11 +25,17 @@ where
     let (collect_tx, collect_rx) = sync_channel::<Receiver<U>>(bound);
     let (result_tx, result_rx) = sync_channel::<U>(bound);
 
-    rayon::spawn_fifo(move || {
+    let map_handler = std::thread::spawn(move || {
         iter.map(|v| {
-            let (s, r) = channel::<U>();
-            collect_tx.send(r).unwrap();
-            (v, s)
+            let (s, mut r) = channel::<U>();
+            loop {
+                r = match collect_tx.try_send(r) {
+                    Ok(_) => break (v, s),
+                    Err(TrySendError::Full(r)) => r,
+                    Err(TrySendError::Disconnected(_)) => panic!("this section is unreachable"),
+                };
+                std::thread::yield_now();
+            }
         })
         .par_bridge()
         .for_each(|(v, s)| {
@@ -29,20 +44,33 @@ where
         });
     });
 
-    rayon::spawn(move || {
+    let sort_handler = std::thread::spawn(move || {
         collect_rx.iter().for_each(|r| {
-            result_tx.send(r.recv().unwrap()).unwrap();
+            let mut result = r.recv().unwrap();
+            loop {
+                result = match result_tx.try_send(result) {
+                    Ok(_) => break,
+                    Err(TrySendError::Full(r)) => r,
+                    Err(TrySendError::Disconnected(_)) => panic!("this section is unreachable"),
+                };
+                std::thread::yield_now();
+            }
         });
     });
 
-    result_rx
+    (
+        result_rx,
+        Waiter {
+            handlers: vec![map_handler, sort_handler],
+        },
+    )
 }
 
 pub fn bounded_parralel_map_iterator<I, F, T, U>(
     bound: usize,
     iter: I,
     f: F,
-) -> impl Iterator<Item = U>
+) -> (impl Iterator<Item = U>, Waiter)
 where
     I: Iterator<Item = T> + Send + 'static,
     F: Fn(T) -> U,
@@ -50,10 +78,14 @@ where
     T: Send,
     U: Send + 'static,
 {
-    bounded_parralel_map(bound, iter, f).into_iter()
+    let (r, w) = bounded_parralel_map(bound, iter, f);
+    (r.into_iter(), w)
 }
 
-pub fn bounded_parralel_map_channel<T, U, F>(bound: usize, f: F) -> (SyncSender<T>, Receiver<U>)
+pub fn bounded_parralel_map_channel<T, U, F>(
+    bound: usize,
+    f: F,
+) -> (SyncSender<T>, Receiver<U>, Waiter)
 where
     F: Fn(T) -> U,
     F: Send + Sync + 'static,
@@ -61,154 +93,42 @@ where
     U: Send + 'static,
 {
     let (source_tx, source_rx) = sync_channel::<T>(0);
-    (
-        source_tx,
-        bounded_parralel_map(bound, source_rx.into_iter(), f),
-    )
-}
-
-pub fn bounded_parralel_map_scope<'a, I, F, T, U>(
-    s: &Scope<'a>,
-    bound: usize,
-    iter: I,
-    f: F,
-) -> Receiver<U>
-where
-    I: Iterator<Item = T> + Send + 'a,
-    F: Fn(T) -> U,
-    F: Send + Sync + 'a,
-    T: Send,
-    U: Send + 'a,
-{
-    let (collect_tx, collect_rx) = sync_channel::<Receiver<U>>(bound);
-    let (result_tx, result_rx) = sync_channel::<U>(bound);
-
-    s.spawn(move |_| {
-        iter.map(|v| {
-            let (s, r) = channel::<U>();
-            collect_tx.send(r).unwrap();
-            (v, s)
-        })
-        .par_bridge()
-        .for_each(|(v, s)| {
-            let r = f(v);
-            s.send(r).unwrap();
-        });
-    });
-
-    s.spawn(move |_| {
-        collect_rx.iter().for_each(|r| {
-            result_tx.send(r.recv().unwrap()).unwrap();
-        });
-    });
-
-    result_rx
-}
-
-pub fn bounded_parralel_map_iterator_scope<'a, I, F, T, U>(
-    s: &Scope<'a>,
-    bound: usize,
-    iter: I,
-    f: F,
-) -> impl Iterator<Item = U>
-where
-    I: Iterator<Item = T> + Send + 'a,
-    F: Fn(T) -> U,
-    F: Send + Sync + 'a,
-    T: Send,
-    U: Send + 'a,
-{
-    bounded_parralel_map_scope(s, bound, iter, f).into_iter()
-}
-
-pub fn bounded_parralel_map_channel_scope<'a, T, U, F>(
-    s: &Scope<'a>,
-    bound: usize,
-    f: F,
-) -> (SyncSender<T>, Receiver<U>)
-where
-    F: Fn(T) -> U,
-    F: Send + Sync + 'a,
-    T: Send + 'a,
-    U: Send + 'a,
-{
-    let (source_tx, source_rx) = sync_channel::<T>(0);
-    (
-        source_tx,
-        bounded_parralel_map_scope(s, bound, source_rx.into_iter(), f),
-    )
+    let (result_rx, waiter) = bounded_parralel_map(bound, source_rx.into_iter(), f);
+    (source_tx, result_rx, waiter)
 }
 
 #[cfg(test)]
 mod tests {
-    use std::{thread::sleep, time::Duration};
+    use std::{sync::mpsc::TrySendError, thread::sleep, time::Duration};
 
-    use rayon::scope;
-
-    use crate::{
-        bounded_parralel_map_channel, bounded_parralel_map_channel_scope,
-        bounded_parralel_map_iterator, bounded_parralel_map_iterator_scope,
-    };
+    use crate::bounded_parralel_map_channel;
 
     #[test]
     fn test_bounded_parralel_map_channel() {
-        let result_rx = {
-            let (source_tx, result_rx) = bounded_parralel_map_channel(2, |x: u64| {
+        let (result_rx, waiter) = {
+            let (source_tx, result_rx, waiter) = bounded_parralel_map_channel(2, |x: u64| {
                 sleep(Duration::from_millis(1000u64 - x * 100));
                 x.to_string()
             });
-            rayon::spawn(move || {
+            std::thread::spawn(move || {
                 (1..10).for_each(|x| {
-                    source_tx.send(x).unwrap();
+                    let mut x = x;
+                    loop {
+                        x = match source_tx.try_send(x) {
+                            Ok(_) => break,
+                            Err(TrySendError::Full(x)) => x,
+                            Err(TrySendError::Disconnected(x)) => x,
+                        };
+                        std::thread::yield_now();
+                    }
                 });
             });
-            result_rx
+            (result_rx, waiter)
         };
 
         result_rx.iter().for_each(|r| {
-            println!("{}", r);
+            println!("case1 {}", r);
         });
-    }
-
-    #[test]
-    fn test_bounded_parralel_map_channel_scope() {
-        scope(|s| {
-            let result_rx = {
-                let (source_tx, result_rx) = bounded_parralel_map_channel_scope(s, 2, |x: u64| {
-                    sleep(Duration::from_millis(1000u64 - x * 100));
-                    x.to_string()
-                });
-                s.spawn(move |_| {
-                    (1..10).for_each(|x| {
-                        source_tx.send(x).unwrap();
-                    });
-                });
-                result_rx
-            };
-
-            result_rx.iter().for_each(|r| {
-                println!("{}", r);
-            });
-        });
-    }
-
-    #[test]
-    fn test_bounded_parralel_map_iterator() {
-        let sut = vec![1, 2, 3, 4, 5, 6, 7];
-
-        let result: Vec<i32> =
-            bounded_parralel_map_iterator(3, sut.into_iter(), |x| x * x).collect();
-        assert!(result == vec![1, 4, 9, 16, 25, 36, 49]);
-    }
-
-    #[test]
-    fn test_bounded_parralel_map_iterator_scope() {
-        scope(|s| {
-            let sut = vec![1, 2, 3, 4, 5, 6, 7];
-
-            let result: Vec<i32> =
-                bounded_parralel_map_iterator_scope(s, 3, sut.into_iter(), |x| x * x).collect();
-            assert!(result == vec![1, 4, 9, 16, 25, 36, 49]);
-        });
+        waiter.wait();
     }
 }
